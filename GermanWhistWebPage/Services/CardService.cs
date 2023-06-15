efbe679b433376618fd8c0e19df8a63d2e853292
grbos <63354316+grbos@users.noreply.github.com>
2023-06-15 17:02:35 +0200

    remove unused function

--- GermanWhistWebPage/Services/CardService.cs
+++ GermanWhistWebPage/Services/CardService.cs
@@ -25,37 +25,6 @@ namespace GermanWhistWebPage.Services
             newList.Shuffle();
             return newList;
         }
-        public static bool IsWinningAgainst(Card card, Card otherCard, Suit trumpSuit, Suit? leadSuit)
-        {
-            if (card.Suit == trumpSuit && otherCard.Suit != trumpSuit)
-            {
-                return true;
-            }
-            if (card.Suit != trumpSuit && otherCard.Suit == trumpSuit)
-            {
-                return false;
-            }
-
-
-            if (card.Suit == trumpSuit && otherCard.Suit == trumpSuit)
-            {
-                return card.Number > otherCard.Number;
-            }
-
-            if (card.Suit == leadSuit && otherCard.Suit != leadSuit)
-            {
-                return true;
-            }
-            if (card.Suit != leadSuit && otherCard.Suit == leadSuit)
-            {
-                return false;
-            }
-            if (card.Suit == leadSuit && otherCard.Suit == leadSuit)
-            {
-                return card.Number > otherCard.Number;
-            }
-            return false;
-        }
 
         public Card getCardFromId(int cardId)
         {
