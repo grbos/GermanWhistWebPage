6565f4a358f3a9196244d9535f2e59fdc479214c
grbos <63354316+grbos@users.noreply.github.com>
2023-06-16 15:07:10 +0200

    add getCards

--- GermanWhistWebPage/Services/CardService.cs
+++ GermanWhistWebPage/Services/CardService.cs
@@ -7,28 +7,29 @@ namespace GermanWhistWebPage.Services
 
     public class CardService
     {
-        private List<Card> _cards;
+
+        public List<Card> Cards { get; private set; }
         public CardService()
         {
-            _cards = new List<Card>();
+            Cards = new List<Card>();
             int j = 0;
             for (int i = 2; i < 15; i++) {
                 foreach (Suit suit in Enum.GetValues(typeof(Suit))) {
-                _cards.Add(new Card(j, suit, i));
+                Cards.Add(new Card(j, suit, i));
                 j += 1;
             } }
         }
 
         public List<int> GetShuffeledCards()
         {
-            var newList = _cards.Select(card => card.Id).ToList();
+            var newList = Cards.Select(card => card.Id).ToList();
             newList.Shuffle();
             return newList;
         }
 
         public Card getCardFromId(int cardId)
         {
-            return _cards[cardId];
+            return Cards[cardId];
         }
     }
 }
