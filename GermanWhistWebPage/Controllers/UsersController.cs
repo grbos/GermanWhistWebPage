bbc3dfa75b05f880ad6f0ba2ece55f16e06622b9
grbos <63354316+grbos@users.noreply.github.com>
2023-06-21 19:04:56 +0200

    Creating users

---
+++ GermanWhistWebPage/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using GermanWhistWebPage.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GermanWhistWebPage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(
+            UserManager<IdentityUser> userManager
+        )
+        {
+            _userManager = userManager;
+        }
+
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _userManager.CreateAsync(
+                new IdentityUser() { UserName = user.UserName, Email = user.Email },
+                user.Password
+            );
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            user.Password = null;
+            return Created("", user);
+        }
+
+
+    }
+}
